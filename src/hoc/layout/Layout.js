f74619c1494e1975f83339dab0876d71ab1ea6d8
max <mkwtzt@gmail.com>
2022-03-22 21:19:15 +0400

    start

---
+++ src/hoc/layout/Layout.js
@@ -0,0 +1,15 @@
+import {Component} from "react";
+
+class Layout extends Component {
+    render() {
+        return (
+            <div>
+                <main>
+                    {this.props.children}
+                </main>
+            </div>
+        )
+    }
+}
+
+export default Layout;
