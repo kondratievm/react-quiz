69c93a6f70c8897d2a407b62cd74d50b5532d65e
max <mkwtzt@gmail.com>
2022-04-15 16:27:25 +0400

    added react dom router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import Layout from "./hoc/layout/Layout";
 import Quiz from './containers/quiz/Quiz';
+import {Route, Switch} from 'react-router-dom';
+import QuizList from "./containers/quizList/quizList";
+import Auth from "./containers/auth/auth";
+import QuizCreator from "./containers/quizCreator/quizCreator";
 
 function App() {
     return (
         <Layout>
-            <Quiz/>
+            <Switch>
+                <Route path='/auth' component={Auth}/>
+                <Route path='/quiz-creator' component={QuizCreator}/>
+                <Route path='/quiz/:id' component={Quiz}/>
+                <Route path='/' component={QuizList}/>
+            </Switch>
         </Layout>
     );
 }
--- src/components/navigation/drawer/drawer.js
+++ src/components/navigation/drawer/drawer.js
@@ -1,17 +1,31 @@
 import s from './drawer.module.css';
 import React, {Component} from "react";
 import Backdrop from "../../ui/backdrop/backdrop";
+import {NavLink} from 'react-router-dom';
 
 const links = [
-    1, 2, 2
+    {to: '/', label: 'Список', exact: true},
+    {to: '/auth', label: 'Авторизация', exact: false},
+    {to: '/quiz-creator', label: 'Создать тест', exact: false},
 ];
 
 class Drawer extends Component {
+    clickHandler = () => {
+        this.props.onClose()
+    }
+
     renderLinks() {
         return links.map((link, index) => {
             return (
                 <li key={index}>
-                    <a href=' '>Link {link}</a>
+                    <NavLink
+                        to={link.to}
+                        exact={link.exact}
+                        activeClassName={s.active}
+                        onClick={this.clickHandler}
+                    >
+                        {link.label}
+                    </NavLink>
                 </li>
             )
         })
